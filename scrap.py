f858b2703d8f113ccc4616143675f53db11939fe
Demokdawa <Demokdawa@yahoo.fr>
2019-06-11 17:11:57 +0200

    SpellCheck updates

--- scrap.py
+++ scrap.py
@@ -6,6 +6,10 @@ page_link = 'https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.ht
 page_response = requests.get(page_link, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
 
-
-resultt = page_content.find_all(string=re.compile("Relic"))
-print(resultt)
+r_relic_list = []
+for text in page_content.findAll('td', string=re.compile("Relic$")):
+    b = text.get_text()
+    r_relic_list.append(b)
+d_relic_list = list(dict.fromkeys(r_relic_list))
+n_relic_list = sorted(d_relic_list)
+print(n_relic_list)
