770f5bffb58f397af73e311232c01caa8e9a11f0
Demokdawa <Demokdawa@yahoo.fr>
2019-06-12 16:55:32 +0200

    SpellCheck updates

--- weblic/relic/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RelicConfig(AppConfig):
-    name = 'relic'
--- weblic/relic/urls.py
+++
@@ -1,9 +0,0 @@
-# relic/urls.py
-from django.conf.urls import url
-from relic import views
-
-
-urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
-]
