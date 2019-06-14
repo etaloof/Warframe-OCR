2340e380144c5796b0c84ad55cf649ebf97b020a
Demokdawa <Demokdawa@yahoo.fr>
2019-06-14 15:55:02 +0200

    Web changes

--- web.py
+++ web.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+import sqlite3
 
 
 @app.route('/')
 def index():
-    return "Hello !"
+    db = sqlite3.connect('relicdb.sqlite3')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM Relic")
+    data = cursor.fetchall()
+    return render_template('test.html', data=data)
+
+
+@app.route('/table')
+def tabledex():
+    db = sqlite3.connect('relicdb.sqlite3')
+    cursor = db.cursor()
+    cursor.execute('''SELECT Relic.Name, Relic.Era, User.Pseudo, Relic.Quality, Relic.Quantity FROM Relic INNER JOIN User ON Relic.IDOwner = User.IDUser''')
+    # cursor.execute("SELECT * FROM Relic")
+    data = cursor.fetchall()
+    return render_template('table.html', data=data)
 
 
 if __name__ == '__main__':
