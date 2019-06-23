5e4901bee0e49840054ceee5bb26385c49d2d66a
Demokdawa <Darkbarjot78>
2019-06-24 01:31:17 +0200

    First stable commit, for now..

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import asyncio
 from utils import *
 from db_operations import *
-import urllib.request
 
 
 # Initialize ##################################################################################
