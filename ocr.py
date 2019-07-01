6ca44214c0d9313ced13779b90ccd536ff5d6102
Demokdawa <Darkbarjot78>
2019-07-01 22:10:19 +0200

    First stable commit, for now..

--- ocr.py
+++ ocr.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from spellcheck import SpellCheck
+import uuid
 
 
 def spell_correction_ocr(string):
@@ -102,6 +103,7 @@ def data_pass_nb(pos1, pos2, pos3, pos4, image, theme):
         img = cv2.dilate(upscaled, kernel, iterations=1)
         kernelled = cv2.erode(img, kernel, iterations=1)
         ret, imgtresh = cv2.threshold(create_mask(theme, kernelled), 218, 255, cv2.THRESH_BINARY_INV)
+        cv2.imwrite('test_img_ocr/' + str(uuid.uuid1()) + '.jpg', imgtresh)
         tessdata_dir_config = '--tessdata-dir "/home/Warframe-OCR/tessdata" -l Roboto --oem 1 -c tessedit_char_whitelist=Xx0123456789 get.images'
         text = pytesseract.image_to_string(imgtresh, config=tessdata_dir_config)
         print(text)
