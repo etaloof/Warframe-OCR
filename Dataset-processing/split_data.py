4a78cfe1dff0355b7eaf789c8cd585e795a459fd
Demokdawa <Demokdawa@yahoo.fr>
2019-10-14 12:37:08 +0200

    Update split_data.py

--- Dataset-processing/split_data.py
+++ Dataset-processing/split_data.py
@@ -8,13 +8,10 @@ def check_image_size(image):
     width = image.shape[1]
     return width, height
 
-count = 0
-
 for img in os.listdir(path):
     img_input = os.path.join(path, img)
     imgdata = cv2.imread(img_input)
     if check_image_size(image) == (1920, 1080)
-        count = count + 1
-        os.mkdir(os.path.join(path, "count"))
+        os.mkdir(os.path.join(path, "img"))
     else
         print("Erreur de la résolution image")
