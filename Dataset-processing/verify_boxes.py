f3e31f189c3417bd7d2148ad1875bb838491c1b8
Demokdawa <Demokdawa@yahoo.fr>
2019-10-16 10:52:06 +0200

    menage

--- Dataset-processing/verify_boxes.py
+++ Dataset-processing/verify_boxes.py
@@ -1,10 +1,28 @@
 import os
+import PySimpleGUI as sg
+from PIL import Image
 
-def parse_data_boxes(input_box):
+def launch_ui(img):
+
+    layout = [
+                  [sg.Text('Entrez le texte'), ],
+                  [sg.InputText(focus=True)],
+                  [sg.Image(img)],
+                  [sg.Submit()]
+                 ]
+
+    window = sg.Window('Botty', layout, keep_on_top=True, use_default_focus=False)
+    event, values = window.Read()
+    window.Close()
+    return values[0]
+
+def parse_data_boxes(input_box, img):
     result = ''
     for line in input_box:
         result += line[0]
-    return result
+    print(result)
+    val = launch_ui(img)
+    print(val)
 
 dirpath = os.path.join(os.getcwd(), "source")
 
@@ -13,5 +31,6 @@ for node_l1 in os.listdir(dirpath):
         for node_l2 in os.listdir(os.path.join(dirpath, node_l1)):
             if node_l2.endswith('.box'):
                 file = open(os.path.join(dirpath, node_l1, node_l2),'r')
-                print(parse_data_boxes(file))
+                img = os.path.join(dirpath, node_l1, node_l2).split('.')[0] + ".jpg"
+                parse_data_boxes(file, img)
             
