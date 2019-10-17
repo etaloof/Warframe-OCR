608f640e57cb29bdfbafad858f70c86902a6ee33
Demokdawa <Demokdawa@yahoo.fr>
2019-10-17 16:45:10 +0200

    update !

--- Dataset-processing/verify_boxes.py
+++ Dataset-processing/verify_boxes.py
@@ -1,6 +1,7 @@
 import os
 import PySimpleGUI as sg
 from PIL import Image
+import itertools as it
 
 def launch_ui(img, result):
 
@@ -17,11 +18,39 @@ def launch_ui(img, result):
     return values[0]
 
 def parse_data_boxes(input_box, img):
+    bx = open(input_box,'r', encoding="utf-8")
     result = ''
-    for line in input_box:
+    for line in bx:
         result += line[0]
     val = launch_ui(img, result)
-    print(val)
+    bx.close()
+    return(val)
+    
+    
+def correct_box_file(input_box, correct_string):
+    bxi = open(input_box,'r', encoding="utf-8")
+    corrected_string = ''
+    box_content = bxi.read()
+    print(box_content)
+    iter = list(it.zip_longest(correct_string, box_content.splitlines(), fillvalue='EndLines'))
+    for x in iter:
+        if x[0][0] == x[1][0]:
+            corrected_string = corrected_string + x[1] + '\n'
+        elif x[1][0] == '\t':
+            corrected_string = corrected_string + x[1] + '\n'
+        elif x[0][0] == 'EndLines':
+            corrected_string = corrected_string + x[1] + '\n'
+        elif x[0][0] != x[1][0]:
+            corr_line = x[0][0] + x[1][1:]
+            corrected_string = corrected_string + corr_line + '\n'
+            
+    bxi.close()       
+    box_corrected = open(input_box + '.boxy', 'w', encoding="utf-8")
+    box_corrected.write(corrected_string)
+    box_corrected.close()
+    
+    
+    
 
 dirpath = os.path.join(os.getcwd(), "source")
 
@@ -29,8 +58,8 @@ for node_l1 in os.listdir(dirpath):
     if os.path.isdir(os.path.join(dirpath, node_l1)):
         for node_l2 in os.listdir(os.path.join(dirpath, node_l1)):
             if node_l2.endswith('.box'):
-                file = open(os.path.join(dirpath, node_l1, node_l2),'r')
+                file_path = os.path.join(dirpath, node_l1, node_l2)
                 img = os.path.join(dirpath, node_l1, node_l2).split('.')[0] + ".png"
-                # parse_data_boxes(file, img)
-                print(file.read())
+                user_str = parse_data_boxes(file_path, img)
+                correct_box_file(file_path, user_str)
             
