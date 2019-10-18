f36405a62c6b4c25cb97a430cf40abc1fc973f45
Demokdawa <Demokdawa@yahoo.fr>
2019-10-18 16:45:32 +0200

    Update verify_boxes.py

--- Dataset-processing/verify_boxes.py
+++ Dataset-processing/verify_boxes.py
@@ -4,26 +4,54 @@ from PIL import Image
 import itertools as it
 from sys import exit
 
-def launch_ui(img, result):
+# Create a list of the img files available to works on
+def create_work_list(dirpath):
+    list = []
+    for node_l1 in os.listdir(dirpath):
+        if os.path.isdir(os.path.join(dirpath, node_l1)):
+            list.append(node_l1)
+    return list
+
+
+# Launch an UI to choose wich img file number to works on
+def launch_menu(choice_list):
+    
+    layout = [
+                  [sg.Text('Choisissez une image a traiter !'), ],
+                  [sg.InputCombo(choice_list, size=(20, 1)), ],     
+                  [sg.Submit()],
+                  [sg.Quit()]
+                 ]
+    window = sg.Window('Choix du fichier', layout, use_default_focus=False)
+    while True:
+        event, values = window.Read()
+        if event == 'Submit':
+            window.Close()
+            return values[0]
+        if event == 'Quit':
+            exit(0)
+
+
+# Launch an UI to let the user correct the box string
+def launch_ui(img, box_text):
 
     layout = [
-                  [sg.Text('Entrez le texte'), ],
-                  [sg.InputText(focus=True, default_text=result)],
+                  [sg.Text('Entrer la bonne valeur !'), ],
+                  [sg.InputText(focus=True, default_text=box_text)],
                   [sg.Image(img)],
                   [sg.Submit()],
                   [sg.Quit()]
                  ]
-
-    window = sg.Window('Botty', layout, keep_on_top=True, use_default_focus=False)
+    window = sg.Window('Verificateur', layout, use_default_focus=False)
     while True:
-        event, values = window.Read() # Run the window until an "event" is triggered
-        if event == "Submit":
+        event, values = window.Read()
+        if event == 'Submit':
+            window.Close()
             return values[0]
-        elif event == "Quit":
-            exit(0)
-        elif event is None or event == "Cancel":
+        if event == 'Quit':
             exit(0)
 
+# Parse a wrong box file string and launch the ui to correct   
 def parse_data_boxes(input_box, img):
     bx = open(input_box,'r', encoding="utf-8")
     result = ''
@@ -34,6 +62,7 @@ def parse_data_boxes(input_box, img):
     return(val)
     
     
+# Create a new file ".boxy" with a corrected box string inside   
 def correct_box_file(input_box, correct_string):
     bxi = open(input_box,'r', encoding="utf-8")
     corrected_string = ''
@@ -55,17 +84,25 @@ def correct_box_file(input_box, correct_string):
     box_corrected.write(corrected_string)
     box_corrected.close()
     
-    
-    
+def main():
 
-dirpath = os.path.join(os.getcwd(), "source")
+    # Generate dirpath to the source img files
+    dirpath = os.path.join(os.getcwd(), "source")
+    
+    # Store the list of imgs files to works in var
+    img_list = create_work_list(dirpath)
 
-for node_l1 in os.listdir(dirpath):
-    if os.path.isdir(os.path.join(dirpath, node_l1)):
-        for node_l2 in os.listdir(os.path.join(dirpath, node_l1)):
+    # Store the img to works on next in var
+    img_choice = launch_menu(img_list)
+                            
+    if os.path.isdir(os.path.join(dirpath, img_choice)): #--> Check if choosen img dir is a dir
+        for node_l2 in os.listdir(os.path.join(dirpath, img_choice)): #--> Loop on the files inside the dir detected
             if node_l2.endswith('.box'):
-                file_path = os.path.join(dirpath, node_l1, node_l2)
-                img = os.path.join(dirpath, node_l1, node_l2).split('.')[0] + ".png"
+                file_path = os.path.join(dirpath, img_choice, node_l2)
+                img = os.path.join(dirpath, img_choice, node_l2).split('.')[0] + ".png"
                 user_str = parse_data_boxes(file_path, img)
                 correct_box_file(file_path, user_str)
+      
             
+if __name__ == "__main__":
+    main()
