350b1ac6be39349600a2fdf5c38d3be3d89d83b8
Demokdawa <Darkbarjot78>
2019-12-12 20:51:34 +0100

    First stable commit, for now..

--- Dataset-processing/slider_ui.py
+++ Dataset-processing/slider_ui.py
@@ -2,38 +2,57 @@ import PySimpleGUI as sg
 from sys import exit
 from PIL import Image
 import base64
+import cv2
 from io import BytesIO
+import numpy as np
 
-# Load IMG as PIL
-image = Image.open("theme_source.png")
+# Load image as numpy format
+image_source = cv2.imread("theme_source.png")
+rgb_source = cv2.cvtColor(image_source, cv2.COLOR_BGR2RGB)
+# Downscale image for interface
+downscale = cv2.resize(rgb_source, (1024, 576), interpolation=cv2.INTER_CUBIC)
+kernel = np.ones((1, 1), np.uint8)
+dilate = cv2.dilate(downscale, kernel, iterations=1)
+kernelled = cv2.erode(dilate, kernel, iterations=1)
+# Convert it to PIL format
+pil_img = Image.fromarray(kernelled)
 
-# Resize it
-image = image.resize((1024, 576))
 
-# Convert it to Base64 string
-buffered = BytesIO()!
-image.save(buffered, format="PNG")
-img_str = base64.b64encode(buffered.getvalue())
+def encode_to_64(img):
+    # Convert image to Base64 string
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str
 
 
-layout = [[sg.Text('TEINTE     '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider1-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider1-up', enable_events=True)],
-          [sg.Text('SATURATION '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider2-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider2-up', enable_events=True)], 
-          [sg.Text('VALEUR     '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider3-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0,255), default_value=0, orientation='h', size=(30,20), key='slider3-up', enable_events=True)], 
-          [sg.Image(data = img_str)],
+def apply_filter(img, slider_dict):
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    lower_virtu = np.array([slider_dict['slider1-down'], slider_dict['slider2-down'], slider_dict['slider3-down']])
+    upper_virtu = np.array([slider_dict['slider1-up'], slider_dict['slider2-up'], slider_dict['slider3-up']])
+    mask = cv2.inRange(hsv, lower_virtu, upper_virtu)
+    ret, imgtresh = cv2.threshold(mask, 218, 255, cv2.THRESH_BINARY_INV)
+    filter_pil_img = Image.fromarray(imgtresh)
+    filter_new_img = encode_to_64(filter_pil_img)
+    return filter_new_img
+
+
+layout = [[sg.Text('TEINTE     '), sg.Slider(range=(0, 255), default_value=0, orientation='h', size=(30, 20), key='slider1-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0, 255), default_value=0, orientation='h', size=(30, 20), key='slider1-up', enable_events=True)],
+          [sg.Text('SATURATION '), sg.Slider(range=(0, 255), default_value=80, orientation='h', size=(30, 20), key='slider2-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0, 255), default_value=255, orientation='h', size=(30, 20), key='slider2-up', enable_events=True)],
+          [sg.Text('VALEUR     '), sg.Slider(range=(0, 255), default_value=80, orientation='h', size=(30, 20), key='slider3-down', enable_events=True), sg.Text(' TO '), sg.Slider(range=(0, 255), default_value=255, orientation='h', size=(30, 20), key='slider3-up', enable_events=True)],
+          [sg.Image(data=encode_to_64(pil_img), key='image')],
           [sg.Quit()]
           ]
 
 # Display the window and get values
-window = sg.Window('Test-app', layout)
+window = sg.Window('Filter-Creator', layout)
 
 while True:
-    event, values= window.Read()
-    window.FindElement('slider1-down').Update(sz)
-    window.FindElement('slider1-up').Update(sz)
-    window.FindElement('slider2-down').Update(sz)
-    window.FindElement('slider2-up').Update(sz)
-    window.FindElement('slider3-down').Update(sz)
-    window.FindElement('slider3-up').Update(sz)
+    event, values = window.Read()
+    new_img = apply_filter(kernelled, values)
+    window.FindElement('image').Update(data=new_img)
+    if event is 'slider1-up' or 'slider1-down' or 'slider2-up' or 'slider2-down' or 'slider3-up' or 'slider3-down':
+        window.FindElement('image').Update(data=new_img)
     if event is None:
         break
     if event == 'Quit':
