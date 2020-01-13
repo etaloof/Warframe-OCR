28d19b5e72d73bf7e732e9c7ca537388b0cde20b
Demokdawa <Demokdawa@yahoo.fr>
2020-01-13 16:07:57 +0100

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -237,8 +237,8 @@ class OcrCheck:
             else:
                 quantity = nb[1:]
                 self.data_pass_name(i[1][1], i[1][3], i[1][0], i[1][2], quantity, self.image, self.theme)
-        # print(self.relic_list)
-        return self.relic_list
+        log.debug(self.relic_list)
+        # return self.relic_list
 
 
 
