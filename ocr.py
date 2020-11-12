698161db6f536c09d34a292876d6c7a4b4e1f6ec
Demokdawa <Airmelie2405>
2020-11-12 15:50:30 +0100

    Updates

--- ocr.py
+++ ocr.py
@@ -2,7 +2,7 @@ import pytesseract
 import cv2
 import numpy as np
 from PIL import Image
-from spellchecker import SpellChecker
+from spellcheck import SpellCheck
 import logging
 import shutil
 import names
